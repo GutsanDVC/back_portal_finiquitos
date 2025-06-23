9391ca56f543562fdf305a838fa635597c019213
GutsanDVC <bastian.gutierrez@dvc.cl>
2025-06-23 18:14:52 -0400

    log de depuración

--- portal_finiquitos/settings.py
+++ portal_finiquitos/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(dotenv_path=BASE_DIR / '.env')
-
+print("DEBUG ENV GOOGLE_AUTH_CLIENT_ID:", os.getenv("GOOGLE_AUTH_CLIENT_ID"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
