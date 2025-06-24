53d2101d8ee1e62fbae87b55d6bf7e0e155cb017
GutsanDVC <bastian.gutierrez@dvc.cl>
2025-06-24 12:49:28 -0400

    Se incluye empresa en query

--- custom_auth/repositories/sql/get_admin_access.sql
+++ custom_auth/repositories/sql/get_admin_access.sql
@@ -1,5 +1,6 @@
 SELECT
  split_part(centro_coto,'-',1) as CC,
+ split_part(sociedad::TEXT,'-',1) as empresa,
  case when ver_planta='x' then true else false end as ver_planta
 FROM flesan_rrhh.tabla_encargados_cc AS tec
 where tec.correo= %(correo)s
